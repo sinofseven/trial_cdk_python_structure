ade4ba8b8fb5d0016f41c431de3b7f50a25502a6
sinofseven <em.s.00001@gmail.com>
2024-04-04 00:50:08 +0900

    Initial commit

---
+++ test/trial_cdk_python_structure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TrialCdkPythonStructure from '../lib/trial_cdk_python_structure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/trial_cdk_python_structure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TrialCdkPythonStructure.TrialCdkPythonStructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
