3c782e3cb16fc34df7a4378f05c5f2425d7d8eec
sinofseven <em.s.00001@gmail.com>
2024-04-04 03:37:44 +0900

    add test

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,7 +1,31 @@
-from pathlib import Path
 import json
+from dataclasses import dataclass, field
+from pathlib import Path
+from uuid import uuid4
 
-from pytest import fixture, MonkeyPatch
+from pytest import MonkeyPatch, fixture
+
+
+def create_uuid() -> str:
+    return str(uuid4())
+
+
+@dataclass
+class DummyLambdaContext:
+    function_name: str = field(default_factory=create_uuid)
+    function_version: str = field(default_factory=create_uuid)
+    invoked_function_arn: str = field(default_factory=create_uuid)
+    memory_limit_in_mb: int = field(default=128)
+    aws_request_id: str = field(default_factory=create_uuid)
+    log_group_name: str = field(default_factory=create_uuid)
+    log_stream_name: str = field(default_factory=create_uuid)
+    identity: None = field(default=None)
+    client_context: None = field(default=None)
+
+
+@fixture(scope="function")
+def dummy_context():
+    return DummyLambdaContext()
 
 
 @fixture(scope="function")
---
+++ tests/unit/layers/common/utils/dataclasses/test_load_environments.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+import pytest
+import utils.dataclasses as index
+
+
+@dataclass
+class SampleClass:
+    country: str
+    capital: str
+
+
+class TestLoadEnvironments:
+    @pytest.mark.parametrize(
+        "set_environments, options, expected",
+        [
+            (
+                {"COUNTRY": "オーブ首長国連邦", "CAPITAL": "オロファト"},
+                {"class_dataclass": SampleClass},
+                SampleClass(country="オーブ首長国連邦", capital="オロファト"),
+            ),
+            (
+                {"COUNTRY": "ユーラシア連邦", "CAPITAL": "モスクワ"},
+                {"class_dataclass": SampleClass},
+                SampleClass(country="ユーラシア連邦", capital="モスクワ"),
+            ),
+        ],
+        indirect=["set_environments"],
+    )
+    @pytest.mark.usefixtures("set_environments")
+    def test_normal(self, options, expected):
+        actual = index.load_environments(**options)
+        assert actual == expected
